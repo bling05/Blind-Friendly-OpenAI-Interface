51d5602c72ceb22140e0a916e83bf3a3a849df4a
Branton Ling <brantonling05@gmail.com>
2023-01-16 20:13:25 -0500

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,8 +1,11 @@
 import discord
 import openai
-from discord.ext import commands
-import asyncio
 from gtts import gTTS
+import soundfile as sf
+from scipy.signal import resample
+
+demon_mode = False
+sample_rates = [8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000]
 
 TOKEN = ''
 CHATGPT_TOKEN = ''
@@ -11,6 +14,18 @@ LANG = 'en'
 TLD = 'com'
 openai.api_key = CHATGPT_TOKEN
 
+def increase_pitch(input_file, pitch_factor):
+    # Load the audio data from the input file
+    data, sr = sf.read(input_file, dtype='int16')
+
+    # Increase the pitch by modifying the sample rate
+    if demon_mode:
+        data = resample(data, int(len(data) * sr / 8000))
+    
+    sr = sample_rates[pitch_factor]
+
+    # Save the modified audio data back to the same file
+    sf.write(input_file, data, sr)
 
 intents = discord.Intents.all()
 intents.members = True
@@ -37,9 +52,29 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global demon_mode
     print(f"{message.author}: {message.content}")
     if message.author == client.user:
         return
+
+    if 'demon mode' in message.content.lower():
+        if demon_mode:
+            await message.channel.send('demon mode off')
+        else:
+            await message.channel.send('demon mode engaged')
+        demon_mode = not demon_mode
+
+    if message.author.id == 330378005161443339:
+        str = message.content
+        if str != '' and message.guild.voice_client is not None:
+            tts = gTTS(text=str, tld=TLD, lang=LANG)
+            tts.save("string_audio.mp3")
+            increase_pitch("string_audio.mp3", 6)
+            vc = client.voice_clients[0]
+            if vc.is_connected():
+                vc.play(discord.FFmpegPCMAudio("string_audio.mp3"))
+                vc.source = discord.PCMVolumeTransformer(vc.source)
+                vc.source.volume = 1
 
     if 'join kat' in message.content.lower() or 'join cat' in message.content.lower():
         if message.author.voice: # If the person is in a channel
@@ -64,17 +99,17 @@ async def on_message(message):
         if str != '':
             await message.channel.send(str)
             
-            if message.guild.voice_client is not None:
-                tts = gTTS(text=str, tld=TLD, lang=LANG)
-                tts.save("string_audio.mp3")
-                vc = client.voice_clients[0]
-                if vc.is_connected():
-                    vc.play(discord.FFmpegPCMAudio("string_audio.mp3"))
-                    vc.source = discord.PCMVolumeTransformer(vc.source)
-                    vc.source.volume = 0.15
-            if message.content.startswith('branton is so awesome (top 133 tank prodigy)'):
-                vc = client.voice_clients[0]
-                vc.play(discord.FFmpegPCMAudio("grind.mp3"))
+            # if message.guild.voice_client is not None:
+            #     tts = gTTS(text=str, tld=TLD, lang=LANG)
+            #     tts.save("string_audio.mp3")
+            #     vc = client.voice_clients[0]
+            #     if vc.is_connected():
+            #         vc.play(discord.FFmpegPCMAudio("string_audio.mp3"))
+            #         vc.source = discord.PCMVolumeTransformer(vc.source)
+            #         vc.source.volume = 1
+            # if message.content.startswith('branton is so awesome (top 133 tank prodigy)'):
+            #     vc = client.voice_clients[0]
+            #     vc.play(discord.FFmpegPCMAudio("grind.mp3"))
 
 
         return
